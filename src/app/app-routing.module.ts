6f818cbe6037f3f37496a5d07b5c4e402160324e
Bajzel <niemczakigor@gmail.com>
2020-09-20 13:11:23 +0200

    routing 20.09

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { NewsComponent } from './views/news/news.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: 'news', component: NewsComponent },
+  { path: '', redirectTo: '/news', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
